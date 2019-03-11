3f76e1f7ea14bf18287c541cc64e618194bb7fca
Dan Hwang <danhwangfkpc@gmail.com>
2019-03-11 10:54:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+
+katz_deli = []
+
+def line(katz_deli)
+	katz_deli.map.with_index {|name, index| puts "The line is currently: #{index}. #{name}"}
+	if katz_deli == []
+		puts "The line is currently empty."
+	end
+end
+
+def take_a_number(katz_deli, name)
+  puts "Welcome, #{name}. You are number #{name.index(name) + 1} in line."
+end
+
+def now_serving(katz_deli)
+  puts "Currently serving "
+end
